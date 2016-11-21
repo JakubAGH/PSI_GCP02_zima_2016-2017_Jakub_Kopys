5be06ce64a65cfeab713cd24b6f8408b9d8943a6
Jejkej <jakub.kopys@tlen.pl>
2016-11-21 08:01:50 +0100

    first commit-zagadnienie1

---
+++ src/LosowanieBezPowtorzenZUsuwaniem.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.LinkedList;
+import java.util.List;
+
+class LosowanieBezPowtorzenZUsuwaniem {
+
+    // Lista liczb
+    private List<Integer> liczby;
+
+    // wielkość początkowa
+    private Integer size;
+
+    // inicjacja obiektu
+    public void init() {
+        // tworzymy zbiór liczb
+        liczby = new LinkedList<Integer>();
+        // dodajemy kolejne liczby do zbioru
+        for (int i = 0; i < this.getSize() + 1; i++) {
+            liczby.add(new Integer(i));
+        }
+    }
+
+    // losowanie
+    public Integer get() {
+        Integer i = (int) (Math.random() * size);
+        i = liczby.get(i);
+        liczby.remove(i);
+        this.size = liczby.size();
+        return i;
+    }
+
+    // zwraca wielkość zbioru
+    private Integer getSize() {
+        return size;
+    }
+
+    // konstruktor jako parametr przyjmujw wielkość zbioru.
+    public LosowanieBezPowtorzenZUsuwaniem(Integer size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException("Wielkość musi być większa od 0");
+        }
+        this.size = size;
+        init();
+    }
+}
---
+++ src/Psi.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+public class Psi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Neuron neuron = new Neuron();
+        neuron.LosujWagi();
+        for (int k = 0; k < 30; k++) {
+            neuron.blad = 0;
+            neuron.info = 0;
+            for (int i = 0; i < 900; i++) {
+                neuron.suma = 0;
+                neuron.WczytajWartosci(i);
+                neuron.Suma();
+                neuron.FunkcjaSkokowa();
+                neuron.UczenieWidrowa(i);
+
+            }
+            System.out.println(neuron.blad / 900.0);
+        }
+
+    }
+
+}
